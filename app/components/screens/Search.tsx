663d36c25cda2d2a8c4e49a9cb428647e5883029
Nik <nik@MacBook-Air-Nikita.local>
2023-08-19 19:32:08 +0300

    create search bar and chips for search screen

--- app/components/screens/Search.tsx
+++ app/components/screens/Search.tsx
@@ -1,8 +1,61 @@
-import { FC } from "react";
-import { View } from "react-native";
+import { FC, useState } from "react";
+import { SafeAreaView, View } from "react-native";
+import { styles } from "../../styles";
+import SearchBar from "react-native-general-searchbar";
+import { Box, Chip, Flex, VStack } from "@react-native-material/core";
 
 const Search: FC = () => {
-    return <View></View>;
+    const [value, setValue] = useState<string | undefined>();
+
+    const onChange = (values: string | undefined) => {
+        setValue(values);
+    };
+
+    const onSubmit = () => {
+        console.log("submitted");
+    };
+
+    const chips = [
+        { id: 1, name: "Галактики" },
+        { id: 2, name: "Планеты" },
+        { id: 3, name: "Спутники" },
+        { id: 4, name: "Метеориты" },
+        { id: 5, name: "Созвездия" }
+    ];
+
+    return (
+        <SafeAreaView style={styles.safeAreaView}>
+            <Box style={styles.defaultBox}>
+                <VStack spacing={50}>
+                    <Box>
+                        <SearchBar
+                            placeholder="Поиск..."
+                            onChangeText={onChange}
+                            onSubmitEditing={onSubmit}
+                            searchText={value}
+                            style={{
+                                inputView: styles.searchInputView,
+                                searchImage: styles.searchInputImage
+                            }}
+                        />
+                    </Box>
+                    <Flex direction="row" wrap="wrap" style={styles.chipsFlex}>
+                        {chips.map((item) => {
+                            return (
+                                <Chip
+                                    label={item.name}
+                                    color="#9191ff"
+                                    onPress={() => {
+                                        console.log(item.id);
+                                    }}
+                                />
+                            );
+                        })}
+                    </Flex>
+                </VStack>
+            </Box>
+        </SafeAreaView>
+    );
 };
 
 export default Search;
---
+++ app/styles/index.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    safeAreaView: {
+        backgroundColor: "white",
+        height: "100%"
+    },
+    defaultBox: {
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    searchInputView: {
+        marginTop: 80,
+        borderWidth: 0,
+        paddingLeft: 10,
+        paddingRight: 10,
+        backgroundColor: "white",
+        height: 50
+    },
+    searchInputImage: {
+        right: 0,
+        position: "absolute",
+        marginRight: 20
+    },
+    chipsFlex: {
+        gap: 5,
+        paddingLeft: 5,
+        paddingRight: 5
+    }
+});
